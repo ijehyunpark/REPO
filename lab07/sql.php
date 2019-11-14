1b968ba4cb76ae4b1863833186a95ccd81d97064
ijehyunpark <adam991217@naver.com>
2019-11-15 00:12:40 +0900

    lab07

--- lab07/sql.php
+++ lab07/sql.php
@@ -5,19 +5,19 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
 <body style="text-align: center;">
-	<div style="display: inline-block; border: 5px solid red; margin-top: 100px; padding: 50px;">
+	<div style="display: inline-block; border: 5px solid red; margin-top: 50px;  margin-bottom: 50px; padding: 50px;">
 		<form action = '' method="post"  >
 			<?php if(empty($_POST['username'])){ ?>
 				<h1>MYSQL</h1>
 
 				<div style="">
-					<input type="text" name="username" placeholder="Input your Mysql username" class="form-control" autocomplete="off"/>
+					<input type="text" name="username" placeholder="Input your Mysql username" value="root" class="form-control" autocomplete="off"/>
 					<hr />
-					<input type="text" name="password" placeholder="Input your Mysql password" class="form-control" autocomplete="off"/>
+					<input type="text" name="password" placeholder="Input your Mysql password" value="root" class="form-control" autocomplete="off"/>
 					<hr />
 					<input type="text" name="DBname" placeholder="Input your DB name" class="form-control" autocomplete="off"/>
 					<hr />
-					<input type="text" name="query" placeholder="Input your SQL query" class="form-control" autocomplete="off"/>
+					<input type="text" name="query" placeholder="Input your SQL query" value="show databases" class="form-control" autocomplete="off"/>
 					<hr />
 					<input type="submit" name="" class="btn btn-primary"/>
 				</div>
@@ -51,7 +51,8 @@
 					}
 					$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 					echo "<div><h1>NOW QUERY :" . $_POST['query'] . "<h1/><div/>";
-					echo "<hr />"; ?>
+					echo "<hr />"; 
+					?>
 					<div style="width: 100%;">
 						<input type="text" name="username" value="<?= $_POST['username']?>" placeholder="Input your Mysql username" class="form-control" autocomplete="off"/>
 						<input type="text" name="password" value="<?= $_POST['password']?>" placeholder="Input your Mysql password" class="form-control" autocomplete="off"/>
@@ -90,7 +91,12 @@
 			</div>
 		</form>
 	</div>
-	<hr />
-	<p>Mysql id,pw 를 입력하고 (query 에'show databases' 입력을 통해 database 검색) 혹은 (database 이름과 query 를 작성하여 mysql의 탐색 기능을 누릘 수 있습니다.)</p>
+	<?php if(empty($_POST['username'])){ ?>	
+		<hr />
+		<p>Mysql id,pw 를 입력하고 (query 에'show databases' 입력을 통해 database 검색) 혹은 (database 이름과 query 를 작성하여 mysql의 탐색 기능을 누릘 수 있습니다.)</p>
+		<hr />
+		<h2>UPDATE</h2>
+		<p>version.1.1:내가 편할려고 기본값 수정</p>
+	<?php } ?>
 </body>
 </html>
